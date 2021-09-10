2c0fcfe72b382b7d4e0616672dd29e2c7d4cef7f
Vira Tamas <vira.tamas97@gmail.com>
2021-09-10 15:40:30 +0200

    Init commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/tomise20/bookings/pkg/config"
+
+	"github.com/tomise20/bookings/pkg/handlers"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+	return mux
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/tomise20/bookings
+
+go 1.17
+
+require (
+	github.com/alexedwards/scs/v2 v2.4.0
+	github.com/go-chi/chi v1.5.4
+	github.com/go-chi/chi/v5 v5.0.4
+	github.com/justinas/nosurf v1.1.1
+)
